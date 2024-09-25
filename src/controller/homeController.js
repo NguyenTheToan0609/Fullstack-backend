8add467c4e5759757e51149275ff2f3d5f963b71
NguyenTheToan0609 <thetoan0609@gmail.com>
2024-09-25 19:22:28 +0700

    connect mysql database with sequelize

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -1,5 +1,9 @@
-let getHomePage = (req, res) => {
-  return res.render("homePage.ejs");
+import db from "../models/index";
+
+let getHomePage = async (req, res) => {
+  let data = await db.User.findAll();
+
+  return res.render("homePage.ejs", { data: JSON.stringify(data) });
 };
 
 module.exports = {
--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ import "dotenv/config";
 import bodyParser from "body-parser";
 import configViewEngine from "./config/viewEngine";
 import initWebRoutes from "./route/web";
+import connectDB from "./config/connectDB";
 
 const app = express();
 const port = process.env.PORT || 6969;
@@ -12,6 +13,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 configViewEngine(app);
 initWebRoutes(app);
+connectDB();
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
