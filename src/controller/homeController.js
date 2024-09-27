1ec68b77e33d920ceb62a9ebb4be0cf9b74c5907
NguyenTheToan0609 <thetoan0609@gmail.com>
2024-09-27 13:05:04 +0700

    CRUD with ORM (update,delete)

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -1,5 +1,11 @@
 import db from "../models/index";
-import { createNewser, getAllUsers } from "../services/CRUDService";
+import {
+  createNewser,
+  getAllUsers,
+  getUserInfoId,
+  updateUserData,
+  deleteUserData,
+} from "../services/CRUDService";
 
 let getHomePage = async (req, res) => {
   let data = await getAllUsers();
@@ -21,9 +27,38 @@ let displayGetCRUD = async (req, res) => {
   return res.render("displayusers.ejs", { dataUsers: data });
 };
 
+let getEditCRUD = async (req, res) => {
+  let userId = req.query.id;
+  if (userId) {
+    let userData = await getUserInfoId(userId);
+    return res.render("editCRUD.ejs", {
+      userData: userData,
+    });
+  }
+};
+
+let putEditCRUD = async (req, res) => {
+  let data = req.body;
+  await updateUserData(data);
+  return res.redirect("/get-crud");
+};
+
+let deleteCRUD = async (req, res) => {
+  let id = req.query.id;
+  if (id) {
+    await deleteUserData(id);
+    return res.redirect("/get-crud");
+  } else {
+    return res.send("Not found id");
+  }
+};
+
 module.exports = {
   getHomePage,
   getCRUD,
   postCRUD,
   displayGetCRUD,
+  getEditCRUD,
+  putEditCRUD,
+  deleteCRUD,
 };
--- src/services/CRUDService.js
+++ src/services/CRUDService.js
@@ -1,6 +1,7 @@
 import bcrypt from "bcryptjs";
 import db from "../models/index";
 import { raw } from "body-parser";
+import { where } from "sequelize";
 
 const salt = bcrypt.genSaltSync(10);
 
@@ -47,7 +48,66 @@ let getAllUsers = () => {
   });
 };
 
+let getUserInfoId = async (userId) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      let user = await db.User.findOne({
+        where: { id: userId },
+        raw: true,
+      });
+      if (user) {
+        resolve(user);
+      } else {
+        resolve({});
+      }
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
+
+let updateUserData = (data) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      let user = await db.User.findOne({
+        where: { id: data.id },
+      });
+      if (user) {
+        user.firstName = data.firstname;
+        user.lastName = data.lastname;
+        user.address = data.address;
+
+        await user.save();
+        resolve();
+      } else {
+        resolve();
+      }
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
+
+let deleteUserData = async (userId) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      let user = await db.User.findOne({
+        where: { id: userId },
+      });
+      if (user) {
+        await user.destroy();
+      }
+      resolve();
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
+
 module.exports = {
   createNewser,
   getAllUsers,
+  getUserInfoId,
+  updateUserData,
+  deleteUserData,
 };
